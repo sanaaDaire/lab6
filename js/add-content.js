f996e97ee6acd47aca4b215a88e512e9ebcc51d4
sanaaDaire <sanaa.daire@icloud.com>
2020-02-07 06:49:35 +0200

    sdsdsd

---
+++ js/add-content.js
@@ -0,0 +1,12 @@
+alert ("GOOD MORNING!");
+confirm ("Are you Reader");
+var name = prompt (" whats your name ?");
+var name = prompt ("Arabic/english ?");
+
+if (age > 20) {
+alert (" Your Age is accept");
+} 
+else 
+{ alert (" Your age is young , SORRY!")
+
+}
